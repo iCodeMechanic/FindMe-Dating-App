d1434c70997fc533ff956e361678b106132fdfa7
Rajan Arora <icodemechanic@gmail.com>
2022-04-18 17:39:53 +0530

    Includes Reactive Form in the Application 1) Validate Inputs client side 2) Custom Validation implements 3) Implements reusable form controls

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ using System.Text;
 using API.Dtos;
 using Microsoft.EntityFrameworkCore;
 using API.Interfaces;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -18,11 +19,13 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
             _context = context;
             _tokenService = tokenService;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -33,21 +36,21 @@ namespace API.Controllers
             {
                 return BadRequest("UserName is taken");
             }
-
+            var user = _mapper.Map<AppUser>(registerDto);
             using var hmac = new HMACSHA512();
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            
+                user.UserName = registerDto.Username.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+                user.PasswordSalt = hmac.Key;
+           
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return new UserDto{
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -68,7 +71,8 @@ namespace API.Controllers
         return new UserDto{
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };;
 }
         private async Task<bool> UserExists(string username)
--- API/Dtos/MemberUpdateDto.cs
+++ API/Dtos/MemberUpdateDto.cs
@@ -7,11 +7,11 @@ namespace API.Dtos
 {
     public class MemberUpdateDto
     {
-        public string Introduction { get; set; }
+        public string? Introduction { get; set; }
         
-        public string LookingFor { get; set; }
+        public string? LookingFor { get; set; }
         
-        public string Interests { get; set; }
+        public string? Interests { get; set; }
         
         public string City { get; set; }
         
